4d3d7d375b29d3e787cacd83d0224301495a9e10
Rohit Panchal <70218029+Rohit-5151@users.noreply.github.com>
2025-05-25 20:19:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRUVXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "~!@#$%^&*(){}[]:;<>_+?|";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword()
+{
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    console.log(password);
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    console.log(password);
+    password += number[Math.floor(Math.random() * number.length)];
+    console.log(password);
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    console.log(password);
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+        console.log(password);
+    }
+
+    passwordBox.value = password; 
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.exeCommand("copy");
+}
